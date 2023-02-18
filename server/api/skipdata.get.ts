79f19cadaead7522f52b958f8318a66c667a902c
Tequilaone <474917094@qq.com>
2023-02-18 15:19:27 +0800

    完成stage和tag

--- server/api/skipdata.get.ts
+++ server/api/skipdata.get.ts
@@ -3,20 +3,19 @@ import axios from "axios";
 
 export default defineEventHandler(async (event) => {
   const query = getQuery(event);
-  const token = fs.readFileSync("./server/api/tagtoken.txt");
+  const token = fs.readFileSync("./server/api/skiptoken.txt");
   const cookie = fs.readFileSync("./server/api/cookie.txt");
 
   const r = await axios({
-    url: "https://ml.corp.kuaishou.com/label/api/datasource/statistic/queryStageStatistic",
-    params: {
+    url: "https://ml.corp.kuaishou.com/label/api/dispatcher/getClearSkipList",
+    method: "post",
+    data: {
       dataSourceId: query.sourceid,
       stageName: query.stagename,
-      start: query.tmstart,
-      end: query.tmstop,
     },
     headers: {
       Cookie: `${cookie.toString()}XSRF-TOKEN=${token.toString()}`,
-      Referer: `https://ml.corp.kuaishou.com/label-frontend/summary?dataSourceId=${query.sourceid}`,
+      Referer: `https://ml.corp.kuaishou.com/label-frontend/skipClear?dataSourceId=${query.sourceid}`,
       "X-XSRF-TOKEN": token.toString(),
     },
   });
@@ -26,7 +25,7 @@ export default defineEventHandler(async (event) => {
     ?.match(/\w{64}/g)
     ?.pop();
   if (newToken) {
-    fs.writeFile("./server/api/tagtoken.txt", newToken, "utf-8", (err) => {
+    fs.writeFile("./server/api/skiptoken.txt", newToken, "utf-8", (err) => {
       if (err) throw err;
     });
   }
--- server/api/stagedata.get.ts
+++ server/api/stagedata.get.ts
@@ -1,23 +1,32 @@
-import axios from 'axios';
+import axios from "axios";
+import fs from "fs";
 
-export default  defineEventHandler(async (event) => {
-  const query = getQuery(event)
-  const r = await axios.get('https://ml.corp.kuaishou.com/label/api/datasource/statistic/getSummarizeStatistic',{
-    params:{
-      dataSourceId:query.sourceid
+export default defineEventHandler(async (event) => {
+  const query = getQuery(event);
+  const token = fs.readFileSync("./server/api/stagetoken.txt");
+  const cookie = fs.readFileSync("./server/api/cookie.txt");
+  // console.log(cookie.toString());
+
+  const r = await axios({
+    url: "https://ml.corp.kuaishou.com/label/api/datasource/statistic/getSummarizeStatistic",
+    params: {
+      dataSourceId: query.sourceid,
     },
-    headers:{
-      Cookie:'_did=web_901942082B653B; clientid=3; did=web_3ebef482cca7f55256c8b42e04174ded; didv=1659179042288; _ga=GA1.2.1592057990.1660300870; hdige2wqwoino=W4w2WsybBniHQBzACiEAQrhF73PDREkK4a578b98; apdid=8a2f0f5e-1869-4268-a3d5-0689a7cd637aefc3511b153f72fc1693e09a1262a84a:1674130877:1; accessproxy_session=a1126ccd-b581-410d-bd7d-bf1eeeb7fa08; JSESSIONID=0791623C18CA9374300C943DE5BCCB68; MMUSESSION=bW11X2F1dGhfdXNlcl9oZWItY2hlbmd5dWVmZW5nODE; MMUSESSION=bW11X2F1dGhfdXNlcl9oZWItY2hlbmd5dWVmZW5nODE; XSRF-TOKEN=DD7F9DE5847419B010015B820F71F79939B8CC1B96C14F6169D95E6B946F18E7',
-      Referer:`https://ml.corp.kuaishou.com/label-frontend/summary?dataSourceId=${query.sourceid}`,
-      'X-XSRF-TOKEN':'DD7F9DE5847419B010015B820F71F79939B8CC1B96C14F6169D95E6B946F18E7'
-    }
-  })
-  // const r = await axios({
-  //   url:'http://114.116.41.110:4002/stagedata',
-  //   params:{
-  //     sourceid:query.sourceid
-  //   }
-  // })
-  console.log(r.data);
-  return r.data
-})
+    headers: {
+      Cookie: `${cookie.toString()}XSRF-TOKEN=${token.toString()}`,
+      Referer: `https://ml.corp.kuaishou.com/label-frontend/summary?dataSourceId=${query.sourceid}`,
+      "X-XSRF-TOKEN": token.toString(),
+    },
+  });
+
+  let newToken = r.headers["set-cookie"]
+    ?.pop()
+    ?.match(/\w{64}/g)
+    ?.pop();
+  if (newToken) {
+    fs.writeFile("./server/api/stagetoken.txt", newToken, "utf-8", (err) => {
+      if (err) throw err;
+    });
+  }
+  return r.data;
+});
